4ec3fc9c7035b41b2e0a608e75b66efebbe824f9
The-PDB <rydercasazza@gmail.com>
2024-10-23 17:24:01 -0700

    Initial commit

---
+++ src/bencode.rs
@@ -0,0 +1,172 @@
+use std::{collections::HashSet, fs, path::PathBuf};
+
+use bendy::decoding::{FromBencode, Object};
+use chrono::{DateTime, Local, TimeZone};
+use sha1::{Digest, Sha1};
+
+type BendyResult<T> = Result<T, bendy::decoding::Error>;
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct Torrent {
+    pub info: Info,
+    pub announce_list: Vec<String>,
+    pub creation_date: Option<DateTime<Local>>,
+    pub comment: Option<String>,
+    pub created_by: Option<String>,
+    pub encoding: Option<String>,
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct Info {
+    pub name: String,
+    pub files: Vec<File>,
+    pub hash: [u8; 20],
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct File {
+    pub length: u64,
+    pub md5sum: Option<String>,
+    pub path: PathBuf,
+}
+
+impl Torrent {
+    pub fn new(path: &str) -> BendyResult<Self> {
+        let file = fs::read(path)?;
+        Torrent::from_bencode(&file)
+    }
+}
+
+impl FromBencode for Torrent {
+    fn decode_bencode_object(object: Object) -> BendyResult<Self>
+    where
+        Self: Sized,
+    {
+        let mut info = None;
+        let mut announce_list = HashSet::new();
+        let mut creation_date = None;
+        let mut comment = None;
+        let mut created_by = None;
+        let mut encoding = None;
+
+        let mut dict = object.try_into_dictionary()?;
+        while let Some(pair) = dict.next_pair()? {
+            match pair {
+                (b"info", value) => info = Some(Info::decode_bencode_object(value)?),
+                (b"announce", value) => {
+                    announce_list.insert(String::decode_bencode_object(value)?);
+                }
+                (b"announce-list", value) => {
+                    let mut list_raw = value.try_into_list()?;
+                    while let Some(value) = list_raw.next_object()? {
+                        let mut tier_list = value.try_into_list()?;
+                        while let Some(value) = tier_list.next_object()? {
+                            announce_list.insert(String::decode_bencode_object(value)?);
+                        }
+                    }
+                }
+                (b"creation date", value) => {
+                    creation_date = Some(
+                        Local
+                            .timestamp_opt(i64::decode_bencode_object(value)?, 0)
+                            .unwrap(),
+                    )
+                }
+                (b"comment", value) => comment = Some(String::decode_bencode_object(value)?),
+                (b"created by", value) => created_by = Some(String::decode_bencode_object(value)?),
+                (b"encoding", value) => encoding = Some(String::decode_bencode_object(value)?),
+                _ => {}
+            }
+        }
+
+        let info = info.expect("Decoding Error: Missing info dictionary");
+        let announce_list = announce_list.into_iter().collect();
+
+        Ok(Self {
+            info,
+            announce_list,
+            creation_date,
+            comment,
+            created_by,
+            encoding,
+        })
+    }
+}
+
+impl FromBencode for Info {
+    fn decode_bencode_object(object: Object) -> BendyResult<Self>
+    where
+        Self: Sized,
+    {
+        let mut name = None;
+        let mut files = None;
+
+        let mut length = None;
+        let mut md5sum = None;
+
+        let mut dict = object.try_into_dictionary()?;
+        while let Some(pair) = dict.next_pair()? {
+            match pair {
+                (b"name", value) => name = Some(String::decode_bencode_object(value)?),
+                (b"files", value) => files = Some(Vec::decode_bencode_object(value)?),
+                (b"length", value) => length = Some(u64::decode_bencode_object(value)?),
+                (b"md5sum", value) => md5sum = Some(String::decode_bencode_object(value)?),
+                _ => {}
+            }
+        }
+
+        let mut hasher = Sha1::new();
+        hasher.update(dict.into_raw()?);
+        let hash = hasher.finalize().into();
+
+        let name = name.expect("Decoding Error: Missing name from torrent info");
+
+        if let Some(files) = files {
+            Ok(Self { name, files, hash })
+        } else {
+            Ok(Self {
+                name,
+                files: vec![File {
+                    length: length.expect("Decoding Error: Missing file length"),
+                    md5sum,
+                    path: PathBuf::new(),
+                }],
+                hash,
+            })
+        }
+    }
+}
+
+impl FromBencode for File {
+    fn decode_bencode_object(object: Object) -> BendyResult<Self>
+    where
+        Self: Sized,
+    {
+        let mut length = None;
+        let mut md5sum = None;
+        let mut path = PathBuf::new();
+
+        let mut dict = object.try_into_dictionary()?;
+        while let Some(pair) = dict.next_pair()? {
+            match pair {
+                (b"length", value) => length = Some(u64::decode_bencode_object(value)?),
+                (b"md5sum", value) => md5sum = Some(String::decode_bencode_object(value)?),
+                (b"path", value) => {
+                    path = Vec::decode_bencode_object(value)?
+                        .into_iter()
+                        .map(|bytes| String::from_utf8(bytes).unwrap())
+                        .collect()
+                }
+                _ => {}
+            }
+        }
+
+        let length = length.expect("Decoding Error: File missing length");
+
+        Ok(Self {
+            length,
+            md5sum,
+            path,
+        })
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+mod bencode;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bencode::{File, Torrent};
+    use chrono::DateTime;
+    use std::{path::PathBuf, str::FromStr};
+
+    #[test]
+    fn test_ubuntu_torrent_decoding() {
+        // Test the decoding of a basic torrent file
+        let torrent_path = "test_data/ubuntu.torrent";
+        let torrent = Torrent::new(torrent_path);
+        assert!(torrent.is_ok(), "Failed to decode the torrent file");
+
+        let torrent = torrent.unwrap();
+        assert_eq!(
+            torrent.info.name,
+            "ubuntu-24.04.1-desktop-amd64.iso".to_owned(),
+            "Failed to decode name"
+        );
+        assert_eq!(
+            torrent.info.files,
+            vec![File {
+                length: 6203355136,
+                md5sum: None,
+                path: PathBuf::from(""),
+            }],
+            "Failed to decode files"
+        );
+        assert_eq!(
+            torrent.info.hash,
+            [74, 63, 94, 8, 188, 239, 130, 87, 24, 237, 163, 6, 55, 35, 5, 133, 227, 51, 5, 153,],
+            "Failed to compute correct hash"
+        );
+        assert_eq!(
+            torrent.announce_list,
+            vec![
+                "https://ipv6.torrent.ubuntu.com/announce".to_owned(),
+                "https://torrent.ubuntu.com/announce".to_owned()
+            ],
+            "Failed to decode announce urls"
+        );
+        assert_eq!(
+            torrent.creation_date,
+            Some(DateTime::from_str("2024-08-29T09:03:35-07:00").unwrap()),
+            "Failed to decode creation date"
+        );
+        assert_eq!(
+            torrent.comment,
+            Some("Ubuntu CD releases.ubuntu.com".to_owned()),
+            "Failed to decode comment"
+        );
+        assert_eq!(
+            torrent.created_by,
+            Some("mktorrent 1.1".to_owned()),
+            "Failed to decode created by"
+        );
+        assert!(torrent.encoding.is_none(), "Failed to decode encoding");
+    }
+}
